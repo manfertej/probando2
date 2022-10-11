cb89c6c9a825df7a33fcaaae83a6324a06041c0c
Alfonso Fernández <58088960+manfertej@users.noreply.github.com>
2022-10-11 12:20:36 +0200

    Update EmpleadoTest.java

--- RRHH/src/pkg/EmpleadoTest.java
+++ RRHH/src/pkg/EmpleadoTest.java
@@ -26,14 +26,92 @@ class EmpleadoTest {
 	void tearDown() throws Exception {
 	}
 
+	/*******************************************************************************************/
+
 	@Test
-	void testCalculoNominaBruta() {
-		fail("Not yet implemented");
+	void pruebaEncargado() {
+		float expected = 2900;
+		float resultado = this.empleado.calculoNominaBruta(TipoEmpleado.Encargado, 1100, 10);
+		assertEquals(expected, resultado);
+	}
+
+
+	@Test
+	void pruebaVendedor() {
+		float expected = 2400;
+		float resultado = this.empleado.calculoNominaBruta(TipoEmpleado.Vendedor, 1100, 10);
+		assertEquals(expected, resultado);
+	}
+
+
+	/*******************************************************************************************/
+
+
+	@Test
+	void pruebaSinPrima() {
+		float expected = 2800;
+		float resultado = this.empleado.calculoNominaBruta(TipoEmpleado.Encargado, 800, 10);
+		assertEquals(expected, resultado);
 	}
 
 	@Test
-	void testCalculoNominaNeta() {
-		fail("Not yet implemented");
+	void pruebaConPrima1() {
+		float expected = 2900;
+		float resultado = this.empleado.calculoNominaBruta(TipoEmpleado.Encargado, 1000, 10);
+		assertEquals(expected, resultado);
+	}
+
+	@Test
+	void pruebaConPrima2() {
+		float expected = 3000;
+		float resultado = this.empleado.calculoNominaBruta(TipoEmpleado.Encargado, 1500, 10);
+		assertEquals(expected, resultado);
+	}
+
+
+	/*******************************************************************************************/
+
+	@Test
+	void pruebaConExtra() {
+		float expected = 2400;
+		float resultado = this.empleado.calculoNominaBruta(TipoEmpleado.Vendedor, 1000, 10);
+		assertEquals(expected, resultado);
+	}
+
+	@Test
+	void pruebaSinExtra() {
+		float expected = 2100;
+		float resultado = this.empleado.calculoNominaBruta(TipoEmpleado.Vendedor, 1000, 0);
+		assertEquals(expected, resultado);
+	}
+
+	/*******************************************************************************************/
+
+
+	@Test
+	void pruebanetaTramo0() {
+		float expected = 2000;
+		float bruto = this.empleado.calculoNominaBruta(TipoEmpleado.Vendedor, 0, 0);
+		float resultado = this.empleado.calculoNominaNeta(bruto);
+		assertEquals(expected, resultado);
+	}	
+
+
+	@Test
+	void pruebanetaTramo15() {
+		float expected = 1870;
+		float bruto = this.empleado.calculoNominaBruta(TipoEmpleado.Vendedor, 1500, 0);
+		float resultado = this.empleado.calculoNominaNeta(bruto);
+		assertEquals(expected, resultado);
+	}	
+
+
+	@Test
+	void pruebanetaTramo18() {
+		float expected = 2050;
+		float bruto = this.empleado.calculoNominaBruta(TipoEmpleado.Encargado, 0, 0);
+		float resultado = this.empleado.calculoNominaNeta(bruto);
+		assertEquals(expected, resultado);
 	}
 
 }
