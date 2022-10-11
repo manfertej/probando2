1518a1d7153081e0fa5cb678a10988a4f0623388
alfonso <manfertej@gmail.com>
2022-10-11 13:34:44 +0200

    Correcciones en la clase Empleado

--- RRHH/src/pkg/EmpleadoTest.java
+++ RRHH/src/pkg/EmpleadoTest.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class EmpleadoTest {
+	
+	private Empleado empleado = new Empleado();
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
